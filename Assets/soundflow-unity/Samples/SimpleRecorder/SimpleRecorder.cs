aeac95ce97f1acd25c192874125783370d21d5fb
xue-fei <awnuxcvbn@163.com>
2025-07-28 22:42:20 +0800

    SimpleRecorder

--- Assets/soundflow-unity/Samples/SimpleRecorder/SimpleRecorder.cs
+++ Assets/soundflow-unity/Samples/SimpleRecorder/SimpleRecorder.cs
@@ -1,22 +1,50 @@
 using SoundFlow.Abstracts;
+using SoundFlow.Abstracts.Devices;
 using SoundFlow.Backends.MiniAudio;
+using SoundFlow.Backends.MiniAudio.Devices;
+using SoundFlow.Backends.MiniAudio.Enums;
 using SoundFlow.Components;
 using SoundFlow.Enums;
+using SoundFlow.Structs;
 using System.IO;
 using UnityEngine;
+using DeviceType = SoundFlow.Enums.DeviceType;
 
 public class SimpleRecorder : MonoBehaviour
 {
-    private AudioEngine? _audioEngine;
+    private AudioEngine  audioEngine;
+   AudioCaptureDevice  captureDevice;
     Recorder recorder;
     FileStream stream;
 
     // Start is called before the first frame update
     void Start()
     {
-        _audioEngine = new MiniAudioEngine(16000, Capability.Record, SampleFormat.F32, 1);
-        stream = new FileStream(Application.streamingAssetsPath + "/output_recording.wav", FileMode.Create, FileAccess.Write, FileShare.None, bufferSize: 4096);
-        recorder = new Recorder(stream, SampleFormat.F32, EncodingFormat.Wav, 16000, 1);
+        audioEngine = new MiniAudioEngine();
+        AudioFormat Format = AudioFormat.Unity;
+        var captureDeviceInfo = SelectDevice(DeviceType.Capture);
+        if (!captureDeviceInfo.HasValue) return;
+        DeviceConfig DeviceConfig = new MiniAudioDeviceConfig
+        {
+            PeriodSizeInFrames = 960, // 10ms at 48kHz = 480 frames @ 2 channels = 960 frames
+            Playback = new DeviceSubConfig
+            {
+                ShareMode = ShareMode.Shared // Use shared mode for better compatibility with other applications
+            },
+            Capture = new DeviceSubConfig
+            {
+                ShareMode = ShareMode.Shared // Use shared mode for better compatibility with other applications
+            },
+            Wasapi = new WasapiSettings
+            {
+                Usage = WasapiUsage.ProAudio // Use ProAudio mode for lower latency on Windows
+            }
+        };
+        captureDevice = audioEngine.InitializeCaptureDevice(captureDeviceInfo.Value, Format, DeviceConfig);
+        captureDevice.Start();
+
+        stream = new FileStream(Application.streamingAssetsPath + "/output_recording.wav", FileMode.Create, FileAccess.Write, FileShare.Read, bufferSize: 4096);
+        recorder = new Recorder(captureDevice, stream, EncodingFormat.Wav);
         recorder.StartRecording();
     }
 
@@ -26,14 +54,39 @@ public class SimpleRecorder : MonoBehaviour
 
     }
 
-    private void OnDestroy()
+    /// <summary>
+    /// Prompts the user to select a single device from a list.
+    /// </summary>
+    private DeviceInfo? SelectDevice(DeviceType type)
     {
+        audioEngine.UpdateDevicesInfo();
+        var devices = type == DeviceType.Playback ? audioEngine.PlaybackDevices : audioEngine.CaptureDevices;
+
+        if (devices.Length == 0)
+        {
+            Debug.Log($"No {type.ToString().ToLower()} devices found.");
+            return null;
+        }
+
+        Debug.Log($"\nPlease select a {type.ToString().ToLower()} device:");
+        for (var i = 0; i < devices.Length; i++)
+        {
+            Debug.Log($"  {i}: {devices[i].Name} {(devices[i].IsDefault ? "(Default)" : "")}");
+        }
+        return devices[1];
+    }
+
+    private void OnDestroy()
+    { 
         recorder.StopRecording();
         stream.Dispose();
-        if (_audioEngine != null)
+        captureDevice.Stop();
+        captureDevice.Dispose();
+
+        if (audioEngine != null)
         {
-            _audioEngine.Dispose();
-            _audioEngine = null;
+            audioEngine.Dispose();
+            audioEngine = null;
         }
     }
 }
